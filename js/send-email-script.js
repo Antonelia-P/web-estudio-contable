56861342d8551d63d22ea5620c9a9610b8746b34
Antonelia Paulini <86538890+antoloky@users.noreply.github.com>
2022-03-14 15:51:12 -0300

    deploy app

--- js/send-email-script.js
+++ js/send-email-script.js
@@ -85,7 +85,7 @@ const form = document.getElementById('form');
             }
 
             //ENVIO EL MAIL
-            window.emailjs.send('service_syd7t47', 'template_9umxa02', tempParams)
+            window.emailjs.send('service_90rtw6h', 'template_ev6hio6', tempParams)
             .then(function(res){
                 console.log('success', res.status);
                 window.location.href="index.html";
